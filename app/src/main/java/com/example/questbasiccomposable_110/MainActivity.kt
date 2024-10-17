f2d36133a64e63611b20b190bb4b790b85941b30
azzamaflah <azzamhebat46@gmail.com>
2024-10-17 15:48:47 +0700

    fix error

--- app/src/main/java/com/example/questbasiccomposable_110/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_110/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable_110.ui.theme.QuestBasicComposable_110Theme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +32,6 @@ class MainActivity : ComponentActivity() {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Basic(modifier = Modifier.padding(innerPadding))
 
-                    )
                 }
             }
         }
@@ -40,7 +47,8 @@ fun Basic(modifier: Modifier = Modifier){
     ){
         Text(text = "Login",
             style = TextStyle(fontSize = 50.sp,
-                fontWeight = FontWeight.Bold))
+                fontWeight = FontWeight.Bold)
+        )
         Text(text = "Halaman login")
         Image(painter = painterResource(
             id = R.drawable.ipul
